7b3b5591ac8d30e72bdeea28928ee5fcc31b55c0
isabel k. lee <hello@isabelklee.co>
2020-03-23 13:16:02 -0400

    created seed data

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+5.times do 
+    Dog.create(name: Faker::Name.name, breed: Faker::Creature::Dog.breed, age: Faker::Number.within(range: 1..15))
+    
+    Walker.create(name: Faker::Name.name)
+
+    Appointment.create(dog_id: Dog.all.sample.id, walker_id: Walker.all.sample.id, date: Faker::Date.forward(days: 60), time: Faker::Time.forward(days: 5,  period: :evening, format: :long))
+end 
