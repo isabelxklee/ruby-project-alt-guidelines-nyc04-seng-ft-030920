7dfb8c0a038b6d9347c5438c1f0fbc2b79376af0
isabel k. lee <hello@isabelklee.co>
2020-03-23 13:17:12 -0400

    added seed data

--- lib/models/appointment.rb
+++ lib/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ActiveRecord::Base
     belongs_to :dog
     belongs_to :walker
+
+    def format_time
+        appt_time = self.time
+        appt_time = "#{appt_time.hour}:#{appt_time.min}"
+    end
+
 end
